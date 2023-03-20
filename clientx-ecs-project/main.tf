db91b99fb661119a1e92f55646565b8707f9175a
attoassi <attot.assi@gmail.com>
2023-03-20 01:30:11 -0400

    ecs service file

--- clientx-ecs-project/main.tf
+++ clientx-ecs-project/main.tf
@@ -60,3 +60,15 @@ module "application_load_balancer" {
   vpc_id                = module.vpc.vpc_id
   certificate_arn       = module.acm.certificate_arn
 }
+
+# Create ecs service
+module "ecs" {
+  source                       = "../modules/ecs"
+  project_name                 = module.vpc.project_name
+  ecs_tasks_execution_role_arn = module.ecs_tasks_execution_role.ecs_tasks_execution_role_arn
+  container_image              = var.container_image
+  region                       = module.vpc.region
+  private_app_subnet_az1_id    = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id    = module.vpc.private_app_subnet_az2_id
+  ecs_security_group_id        = module.security_group.ecs_security_group_id
+}
---
+++ modules/ecs/variables.tf
@@ -0,0 +1,7 @@
+variable "project_name" {}
+variable "ecs_tasks_execution_role_arn" {}
+variable "container_image" {}
+variable "region" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "ecs_security_group_id" {}
